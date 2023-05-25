129da228cf7a3dd4b780cbd6d93379ee16b51607
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2023-05-25 14:09:13 +0900

    Add navigation bar Add CSV import Add users table

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   resources :users
   resources :work_hours
   resources :assignments
-  resources :teaching_assistants
-  resources :courses
+  resources :teaching_assistants do
+    collection {post :import}
+  end
+  resources :courses do
+    collection {post :import}
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_19_085841) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_053541) do
   create_table "assignments", force: :cascade do |t|
     t.integer "course_id", null: false
     t.integer "teaching_assistant_id", null: false
@@ -40,6 +40,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_085841) do
     t.string "grade"
     t.string "labo"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "account_name"
+    t.string "password"
+    t.string "admin_flag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
