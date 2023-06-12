cda790c14c9161e34c75951c81dc4a361b065638
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2023-06-12 10:22:20 +0900

    Update Import

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,13 +3,6 @@ class Course < ApplicationRecord
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      course = find_or_initialize_by(id: row[0])
-      course.assign_attributes(year: row[1].to_i, term: row[2], number: row[3], name: row[4], instructor: row[5], time_budget: row[6], description: row[7])
-      course.save unless course.persisted?
-    end
-  end
-  def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
       course = find_by(id: row[0])
       if course
         course.update(year: row[1].to_i, term: row[2], number: row[3], name: row[4], instructor: row[5], time_budget: row[6], description: row[7])
