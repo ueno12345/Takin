4a7e6aa11c5b70985c240f7d92f1ed6e34eae052
Thikaru <T.hikaru1823@gmail.com>
2023-07-11 19:39:02 +0900

    add commnet excel

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -12,6 +12,9 @@ class AssignmentsController < ApplicationController
 
   # GET /assignments/1 or /assignments/1.json
   def show
+    # ============#
+    # 使用してない =#
+    # ============#
   end
 
   # GET /assignments/new
@@ -112,8 +115,6 @@ class AssignmentsController < ApplicationController
       file_path = Rails.root.join('public', 'excel','writed_form2.xlsx')
       send_file file_path, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment'
     end
-    # redirect_to teaching_assistant_path(@teaching_assistant), notice: "CSVOKまる？？"
-    # send_file("public/excel/test100.xlsx", filename: 'test.xlsx', type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment')
   end
 
   private
@@ -123,15 +124,28 @@ class AssignmentsController < ApplicationController
       params.require(:assignment).permit(:course_id, :teaching_assistant_id, :description)
     end
 
+    # 帳票の様式1を書き込む処理を行うメソッド
     def WriteForm1
-       # ============== #
-      # 様式1作成
-      # ============== #
-      file_change_count = 1
+      # 書き込むファイル名 勤務予定が15個ごと書き込んだエクセルファイルを作成するので，ファイル名を変えたい．保存ファイル名の変えない部分
       first = "public/excel/writed_form1"
+      # ファイル名を変更するためのマジックナンバー
+      file_change_count = 1
+
+      # 様式1のエクセルファイルを読み込む
       file_form1 = "public/excel/form1.xlsx"
+      # エクセルを読み込む処理
       workbook_form1 = RubyXL::Parser.parse(file_form1)
+      # エクセルの書き込みを行うシートを指定「0からスタートする」
       worksheet_form1 = workbook_form1[1]
+
+      # 書き込みを行いたいTAが持っている割当をDBから持ってくる
+      @assignments = Assignment.where(teaching_assistant_id: @teaching_assistant.id)
+      # 今書き込んでいる割当の数-1
+      count = 0;
+      # 合計勤務時間
+      sum_work_time = 0
+      # 曜日を変換するための配列
+      days = ["日", "月", "火", "水", "木", "金", "土"]
 
       # 氏名追加処理
       worksheet_form1.add_cell(9, 20, @teaching_assistant.name)
@@ -141,26 +155,20 @@ class AssignmentsController < ApplicationController
       # 学生番号追加処理
       worksheet_form1.add_cell(18, 21, @teaching_assistant.number)
 
-      # 所属分岐
+      # 所属分岐　学年ごとに分岐されている
       if @teaching_assistant.grade == "M1" or @teaching_assistant.grade == "M2" then
         worksheet_form1.add_cell(20, 15, @teaching_assistant.grade)
         worksheet_form1.add_cell(19, 2, "大学院環境生命自然科学研究科")
         worksheet_form1.add_cell(19, 15, "前")
-
       elsif @teaching_assistant.grade == "D1" or @teaching_assistant.grade == "D2" or @teaching_assistant.grade == "D3" then
         worksheet_form1.add_cell(20, 15, @teaching_assistant.grade)
         worksheet_form1.add_cell(19, 2, "大学院環境生命自然科学研究科")
         worksheet_form1.add_cell(19, 15, "後")
-
       else
         worksheet_form1.add_cell(22, 14, @teaching_assistant.grade)
       end
 
       # 割り当てられた講義
-      @assignments = Assignment.where(teaching_assistant_id: @teaching_assistant.id)
-      count = 0;
-      sum_work_time = 0
-      days = ["日", "月", "火", "水", "木", "金", "土"]
       @assignments.each do |assignment|
         @work_hours = WorkHour.where(assignment_id: assignment.id)
         @course = Course.where(id: assignment.course_id)
@@ -212,29 +220,29 @@ class AssignmentsController < ApplicationController
         end
       end
 
+      # 複数ファイルを書き込んでいくときに最後に書き込み終了して保存するファイル
       #workbook_form1.write("#{first}-#{file_change_count}.xlsx")
       workbook_form1.write("#{first}.xlsx")
 
-      # file_path = Rails.root.join('public', 'excel', 'form2.xlsx')
-      # send_file file_path, filename: 'form2.xlsx', type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-
-      #file_path = "./public/excel/form2.xlsx"
-      # puts "================"
-      # puts  file_path
-      # puts "================"
       # file_path = Rails.root + "#{first}-#{file_change_count}.xlsx"
-      # file_path = Rails.root + "/public/excel/form2.xlsx"
       #send_file(file_path, filename: 'form2.xlsx', type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', disposition: 'attachment')
     end
 
+
+    # 帳票の様式2を書き込む処理を行うメソッド
     def WriteForm2
-      # ============== #
-      # 様式2作成
-      # ============== #
       file_form2 = "public/excel/form2.xlsx"
       workbook_form2 = RubyXL::Parser.parse(file_form2)
       worksheet_form2 = workbook_form2[0]
-
+      # 割り当てられた講義
+      @assignments = Assignment.where(teaching_assistant_id: @teaching_assistant.id)
+      count = 0;
+      sum_work_time = 0
+      days = ["日", "月", "火", "水", "木", "金", "土"]
+      months_start = { 1 => [47,7], 2  => [47,14], 3  => [47,21], 4  => [15,0], 5  => [15,7], 6  => [15,14], 7  => [15,21], 8  => [31,0], 9  => [31,7], 10  => [31,14], 11  => [31,21], 12  => [47,0]}
+      months_time = [0,0,0,0,0,0,0,0,0,0,0,0]
+      day_of_week = [6,0,1,2,3,4,5]
+      add_num = 0
       teachers = ""
 
       # 氏名追加処理
@@ -252,29 +260,17 @@ class AssignmentsController < ApplicationController
         worksheet_form2.add_cell(1, 18, "大学院環境生命自然科学研究科")
         worksheet_form2.add_cell(8, 4, "TA")
         worksheet_form2.add_cell(3, 18, "1")
-
       elsif @teaching_assistant.grade == "D1" or @teaching_assistant.grade == "D2" or @teaching_assistant.grade == "D3" then
         # worksheet_form1.add_cell(20, 15, @teaching_assistant.grade)
         worksheet_form2.add_cell(1, 18, "大学院環境生命自然科学研究科")
         worksheet_form2.add_cell(8, 4, "TA")
         worksheet_form2.add_cell(3, 18, "2")
-
       else
         worksheet_form2.add_cell(8, 4, "SA")
         worksheet_form2.add_cell(3, 18, "なし")
       end
 
-      # 割り当てられた講義
-      @assignments = Assignment.where(teaching_assistant_id: @teaching_assistant.id)
-      count = 0;
-      sum_work_time = 0
-      days = ["日", "月", "火", "水", "木", "金", "土"]
-      months_start = { 1 => [47,7], 2  => [47,14], 3  => [47,21], 4  => [15,0], 5  => [15,7], 6  => [15,14], 7  => [15,21], 8  => [31,0], 9  => [31,7], 10  => [31,14], 11  => [31,21], 12  => [47,0]}
-      months_time = [0,0,0,0,0,0,0,0,0,0,0,0]
-      day_of_week = [6,0,1,2,3,4,5]
-      add_num = 0
-
-
+      # 勤務時間を記入
       @assignments.each do |assignment|
         @work_hours = WorkHour.where(assignment_id: assignment.id)
         @course = Course.where(id: assignment.course_id)
@@ -296,14 +292,6 @@ class AssignmentsController < ApplicationController
 
           line = months_start[month][0] + week_count*2
           row = months_start[month][1]+day_of_week[work_hour.dtstart.wday]
-
-          # puts "==============="
-          # puts work_hour.dtstart
-          # puts "line #{line}"
-          # puts "row #{row}"
-          # puts "week_count #{week_count}"
-          # puts "add_num #{add_num}"
-          # puts "==============="
           
           if  worksheet_form2[line][row].value != "" then
             writed_data = worksheet_form2[line][row].value
@@ -314,9 +302,7 @@ class AssignmentsController < ApplicationController
           count += 1
         end
       end
-
       worksheet_form2.add_cell(5, 18, teachers)
-
       workbook_form2.write("public/excel/writed_form2.xlsx")
     end
 end
