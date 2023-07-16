de6b808d2c12a2060cb5433e6a6624c316b24bef
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2023-07-16 21:12:25 +0900

    Separate date and time

--- app/models/work_hour.rb
+++ app/models/work_hour.rb
@@ -1,9 +1,24 @@
 class WorkHour < ApplicationRecord
   belongs_to :assignment
   validates :dtstart, presence: true
-  validates :dtend, comparison: { greater_than: :dtstart, message: "%{attribute}は開始時刻後にして下さい" }, presence: true
-
+  validates :dtend, comparison: { greater_than: :dtstart, message: "%{attribute}は開始時刻よりも後にして下さい" }, presence: true
   validates :actual_working_minutes, numericality: {greater_than: 0}, presence: true
+
+  def date=(date)
+    @date = date
+  end
+
+  def date
+    @date || dtstart.try(:to_date)
+  end
+
+  def dtstart=(time)
+    self[:dtstart] = time.is_a?(String) ? Time.zone.parse(time) : time
+  end
+
+  def dtend=(time)
+    self[:dtend] = time.is_a?(String) ? Time.zone.parse(time) : time
+  end
 
   def self.ransackable_attributes(auth_object = nil)
     ["actual_working_minutes", "assignment_id", "created_at", "dtend", "dtstart", "id", "updated_at"]
