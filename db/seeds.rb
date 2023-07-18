021be627ae61ddebc57edeb69c0a953638ec8f9f
Callum Tulloch <callumtulloch1024@gmail.com>
2023-07-19 00:12:42 +0900

    微修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,20 @@ flag = true
 
 hashed_password = BCrypt::Password.create(password)
 
-# User.create!(
-#       account_name: name, 
-#       password_digest: hashed_password,
-#       admin_flag: flag
-#     )
+User.create!(
+      account_name: name, 
+      password_digest: hashed_password,
+      admin_flag: flag
+    )
 
-# TeachingAssistant.create!(
-#   year: "00",
-#   number: "00000000",
-#   name: "ダミー君",
-#   grade:"Z1", 
-#   labo:"山内研究室",
-#   description:"ダミーデータやで"
-# )
+ TeachingAssistant.create!(
+   year: "0001",
+   number: "00000000",
+   name: "ダミー君",
+   grade:"M1", 
+   labo:"山内研究室",
+   description:"ダミーデータやで"
+ )
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -31,28 +31,28 @@ hashed_password = BCrypt::Password.create(password)
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-3.times do |n|
-	TeachingAssistant.create!(
-		year: 2023,
-		number: "50M2300#{n}",
-		name: "TA#{n}",
-		grade: "M1",
-		labo: "TEACHER#{n}" ,
-		description: "TEST#{n}"
-	)
-end
-
-3.times do |n|
-	Course.create!(
-		year: 2023,
-		term: "#{n}",
-		number: "09000#{n}",
-		name: "Course#{n}",
-		instructor: "Teacher#{n}",
-		time_budget: (n+1)*1000,
-		description: "TTTT#{n}",
-	)
-end
+#3.times do |n|
+#	TeachingAssistant.create!(
+#		year: 2023,
+#		number: "50M2300#{n}",
+#		name: "TA#{n}",
+#		grade: "M1",
+#		labo: "TEACHER#{n}" ,
+#		description: "TEST#{n}"
+#	)
+#end
+#
+#3.times do |n|
+#	Course.create!(
+#		year: 2023,
+#		term: "#{n}",
+#		number: "09000#{n}",
+#		name: "Course#{n}",
+#		instructor: "Teacher#{n}",
+#		time_budget: (n+1)*1000,
+#		description: "TTTT#{n}",
+#	)
+#end
 
 # Course1にTA1を割り当てる．
 # Assignment.create{
