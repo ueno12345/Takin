b257920f9e06433d9832c46c372d850f376b4ee2
ueno12345 <ueno2022@s.okayama-u.ac.jp>
2023-07-22 23:27:27 +0900

    Add user's validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-    has_secure_password
 
     validates :account_name,
     length: {maximum: 64, too_long: "%{attribute}は%{count}文字以下で入力してください"},
     presence: true
+    has_secure_password
+    validates :password,
+    format: {with: /\A[a-zA-Z0-9]\z/, message: "%{attribute}は半角英数字で入力してください"}
 end
