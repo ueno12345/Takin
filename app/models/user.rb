f8f2d8111af016047e77a13360306296136692a3
Callum Tulloch <callumtulloch1024@gmail.com>
2023-07-23 04:12:48 +0900

    user validate修正．form修正．コメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     validates :account_name,
-    uniqueness: true,
+    uniqueness: { unless: Proc.new { User.count.zero? } },
     length: {maximum: 64, too_long: "%{attribute}は%{count}文字以下で入力してください"},
     format: {with: /\A[a-zA-Z0-9]+\z/, message: "%{attribute}は半角英数字で入力してください"},
     presence: true
